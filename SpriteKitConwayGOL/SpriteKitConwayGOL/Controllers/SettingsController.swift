dae9dcbd68e252af43d415807440ab762c9c6446
Christopher DeVito <christopher.devito@protonmail.com>
2020-06-29 04:31:22 -0400

    Finished setting up SettingsViewController, and set up SettingsController with singleton to pass settings between tabs. Test with temporary labels.

--- SpriteKitConwayGOL/SpriteKitConwayGOL/Controllers/SettingsController.swift
+++ SpriteKitConwayGOL/SpriteKitConwayGOL/Controllers/SettingsController.swift
@@ -6,8 +6,20 @@
 //  Copyright © 2020 Christopher Devito. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class SettingsController {
 
+    static let shared = SettingsController()
+
+    let liveCellColors = ["TEAL", "BLUE", "GREEN", "YELLOW", "RED", "PURPLE"]
+    let liveCellColorsSystem: [UIColor] = [.systemTeal, .systemBlue, .systemGreen, .systemYellow, .systemRed, .systemPurple]
+    var liveCellColor: UIColor = .systemTeal
+    let deadCellColors = ["BLACK", "WHITE", "LIGHT GRAY", "DARK GRAY"]
+    let deadCellColorsSystem: [UIColor] = [.black, .white, .systemGray4, .systemGray]
+    var deadCellColor: UIColor = .white
+    let presets = ["Random", "Gosper Glider Gun", "Constructor", "Stable Shapes", "Spaceships"]
+    var speed: Int = 4
+    var cellSize: Int = 10
+    var preset: String = "Random"
 }
--- SpriteKitConwayGOL/SpriteKitConwayGOL/View Controllers/GameOfLifeViewController.swift
+++ SpriteKitConwayGOL/SpriteKitConwayGOL/View Controllers/GameOfLifeViewController.swift
@@ -9,10 +9,68 @@
 import UIKit
 
 class GameOfLifeViewController: UIViewController {
+    let settingsController = SettingsController.shared
 
+    let stackView = UIStackView()
+    let aliveLabel = UILabel()
+    let deadLabel = UILabel()
+    let speedLabel = UILabel()
+    let cellSizeLabel = UILabel()
+    let presetLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(aliveLabel)
+        stackView.addArrangedSubview(deadLabel)
+        stackView.addArrangedSubview(speedLabel)
+        stackView.addArrangedSubview(cellSizeLabel)
+        stackView.addArrangedSubview(presetLabel)
+        stackView.axis = .vertical
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        aliveLabel.translatesAutoresizingMaskIntoConstraints = false
+        deadLabel.translatesAutoresizingMaskIntoConstraints = false
+        speedLabel.translatesAutoresizingMaskIntoConstraints = false
+        cellSizeLabel.translatesAutoresizingMaskIntoConstraints = false
+        presetLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            stackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.8)
+        ])
+
+        aliveLabel.text = "\(settingsController.liveCellColor)"
+        deadLabel.text = "\(settingsController.deadCellColor)"
+        speedLabel.text = "\(settingsController.speed)"
+        cellSizeLabel.text = "\(settingsController.cellSize)"
+        presetLabel.text = "\(settingsController.preset)"
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NSLog("View Will Appear")
+        aliveLabel.text = "\(settingsController.liveCellColor)"
+        deadLabel.text = "\(settingsController.deadCellColor)"
+        speedLabel.text = "\(settingsController.speed)"
+        cellSizeLabel.text = "\(settingsController.cellSize)"
+        presetLabel.text = "\(settingsController.preset)"
+        view.reloadInputViews()
+    }
+
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        NSLog("View Did Appear")
+//        aliveLabel.text = "\(settingsController.liveCellColor)"
+//        deadLabel.text = "\(settingsController.deadCellColor)"
+//        speedLabel.text = "\(settingsController.speed)"
+//        cellSizeLabel.text = "\(settingsController.cellSize)"
+//        presetLabel.text = "\(settingsController.preset)"
+//        view.reloadInputViews()
+//
+//    }
 }
